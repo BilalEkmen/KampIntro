11d301abcdcd621d4780040e7a236ecf53bfce3e
bilal <bilalekmenPC@gmail.com>
2021-12-14 21:21:55 +0300

    16 ocak ders sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product //just property (özellik)
+    { 
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        //birim fiyatı
+        public double UnitPrice { get; set; }
+        //stok adedi
+        public int UnitInStock { get; set; }
+
+
+        // CRUD = Create - Reade - Update - Delete   
+
+
+
+    }
+}
